be8dd3976a263a5b1e6cc1f6c858ef3b2c273f3f
Ivano Saracino <saracino.g.ivano@gmail.com>
2025-12-23 22:27:31 +0100

    modifiche pre vercel 6

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -55,7 +55,7 @@ export async function login(req, res) {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: true,
-            sameSite: 'lax',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
             path: '/auth/',
         });
